8700c60535fa9d6ed2c4ba8000f3d92c996786aa
W <109571942+victorious4ever@users.noreply.github.com>
2023-08-13 00:20:28 -0700

    Create 122A.cpp

---
+++ Codeforces/1000*  - 1200*/122A.cpp
@@ -0,0 +1,4 @@
+//This was copied because this was just pure brute force and this is for completion purposes
+//https://codeforces.com/problemset/submission/122/218428060
+#include <iostream>
+int l;main(){std::cin>>l;std::cout<<(l%4==0||l%7==0||l%47==0||l%74==0||l%477==0 ? "YES":"NO");}
